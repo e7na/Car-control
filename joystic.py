e4cdf9487c7cbbb5121b91efe79c4da9ac306331
Eslam-Fawzi <fwzya168@gmail.com>
2023-04-22 15:03:40 +0200

    Done with creating frame sent to Nodemcu

--- joystic.py
+++ joystic.py
@@ -1,3 +1,10 @@
+######################################################################
+##############    Joystic Calebration    #############################
+##############        Eslam Fawzi        #############################
+##############        version 1.2        #############################
+##############         22/4/2023         #############################
+######################################################################
+
 import pygame
 import websockets
 import asyncio
@@ -5,10 +12,9 @@ import asyncio
 
 #import requests
 
+#initialze pygame and Joystic
 pygame.joystick.init()
-
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-
 pygame.init()
 JOYSTIC = pygame.joystick.Joystick(0)
 
@@ -20,7 +26,7 @@ async def connect(msg):
       await websocket.send(msg)
       
 
-# maping function to convert analog from -1 , 1 to 0 , 255
+# maping function to convert analog from -1 , 1 to 0 , X (any value needed)
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
@@ -35,6 +41,8 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
 
 
 send_request = ""
+speed = 0
+back_spd=0 
 program = True
 
 #program loop
@@ -82,10 +90,11 @@ while program:
              print(events)'''
 
         if event.type == pygame.JOYBUTTONDOWN:
-            # L1 is used For stop car
+            # R1 is used to stop car
             if JOYSTIC.get_button(10):
-                send_request = "R1_Stop"
-                print(send_request)
+                #creat frame 
+                send_request = "10000"
+                print("R1_Stop_" + send_request)
          
                #Send data
                 asyncio.get_event_loop().run_until_complete(connect(send_request))
@@ -93,8 +102,9 @@ while program:
 
             # shutdown and stop car using PS button
             if JOYSTIC.get_button(5):
-                send_request = "PS_Quit"
-                print(send_request)
+                #creat frame 
+                send_request = "10000"
+                print("PS_Quit_" + send_request)
                 program = False
                 #Send data
                 asyncio.get_event_loop().run_until_complete(connect(send_request))
@@ -104,24 +114,55 @@ while program:
         # تظبيط الانالوج علشان مهم
         elif event.type == pygame.JOYAXISMOTION and not JOYSTIC.get_button(10):
            
+            if JOYSTIC.get_axis(4) > -1 and JOYSTIC.get_axis(5) > -1 :
+                send_request = "10000"
+                print("RL_Stop" + send_request)
+                #Send data
+                asyncio.get_event_loop().run_until_complete(connect(send_request))
+            # L2 is used for revers motion range 0-6
+            elif JOYSTIC.get_axis(4) > -1:
+                L2 = JOYSTIC.get_axis(4)
+                back_spd = translate(L2, -1, 1, 0, 6)
+                speed = 0 
+                #creat frame 
+                send_request = "00" + str(int(back_spd)) + "00"
+                print("L2_" + send_request)
+                #Send data
+                asyncio.get_event_loop().run_until_complete(connect(send_request))
+                
 
-           # Left analog is used for direction range 0-255 right and same to left
+
+            # R2 is used for forward motion range 0-6
+            elif JOYSTIC.get_axis(5) > -1:
+                R2 = JOYSTIC.get_axis(5)
+                speed = translate(R2, -1, 1, 0, 6)
+                back_spd = 0 
+                #creat frame 
+                send_request = "0" + str(int(speed)) + "000"
+                print("R2_" + send_request)
+                #Send data
+                asyncio.get_event_loop().run_until_complete(connect(send_request))
+                
+            # Left analog is used for direction range 0-255 right and same to left
             if JOYSTIC.get_axis(0):
                 state = JOYSTIC.get_axis(0)
                 if state > .01:
                     Right = translate(state, 0, 1, 0, 255)
-                    send_request = "AR_" + str(int(Right)).zfill(3)
-                    print(send_request)
+                    Right = translate(Right , 0 , 255 , 0 , 6 )
+                    #creat frame 
+                    send_request = "0"+ str(int(speed)) + str(int(back_spd)) + str(int(Right)) + "0"
+                    print("AR_" + send_request)
                     #Send data
                     asyncio.get_event_loop().run_until_complete(connect(send_request))
                     
 
                 elif state < 0:
-                    Left = translate(state, 0, -1, 0, 255)
-                    send_request = "AL_" + str(int(Left)).zfill(3)
+                    Left = translate(state, 0, -1, 0, 6)
+                    #creat frame 
+                    send_request = "0" + str(int(speed)) + str(int(back_spd)) + "0" + str(int(Left))
                     #Send data
                     asyncio.get_event_loop().run_until_complete(connect(send_request))
-                    print(send_request)
+                    print("AL_" + send_request)
 
             '''if JOYSTIC.get_axis(1) > .5 and JOYSTIC.get_axis(0) < -.5:
                 print ("down left")  
@@ -136,32 +177,12 @@ while program:
                 print ("UP Left")  
 '''
 
-            # L2 is used for revers motion range 0-255
-            if JOYSTIC.get_axis(4) > -1:
-                L2 = JOYSTIC.get_axis(4)
-                L2 = translate(L2, -1, 1, 0, 255)
-                send_request = "L2_" + str(int(L2)).zfill(3)
-                print(send_request)
-                #Send data
-                asyncio.get_event_loop().run_until_complete(connect(send_request))
-                
-
-
-            # R2 is used for forward motion range 0-255
-            elif JOYSTIC.get_axis(5) > -1:
-                R2 = JOYSTIC.get_axis(5)
-                R2 = translate(R2, -1, 1, 0, 255)
-                send_request = "R2_" + str(int(R2)).zfill(3)
-                print(send_request)
-                #Send data
-                asyncio.get_event_loop().run_until_complete(connect(send_request))
-                
 
 
            # check if joystic disconnected
         if event.type == pygame.JOYDEVICEREMOVED:
-            send_request = "Q_Stop"
-            print(send_request)
+            send_request = "10000"
+            print("Q_Stop" + send_request)
             program = False
             #Send data
             asyncio.get_event_loop().run_until_complete(connect(send_request))
